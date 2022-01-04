256d43a5f524a6ef64933d70451113bee08583c5
steelekreis <steele.kreis@gmail.com>
2022-01-03 17:33:47 -0700

    more cleanup, like seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,5 @@
 const seedUsers = require('./user-seeds');
 const seedPosts = require('./post-seeds');
-const seedComments = require('./comment-seeds');
-const seedVotes = require('./vote-seeds');
 
 const sequelize = require('../config/connection');
 
@@ -12,12 +10,6 @@ const seedAll = async () => {
   console.log('--------------');
 
   await seedPosts();
-  console.log('--------------');
-
-  await seedComments();
-  console.log('--------------');
-
-  await seedVotes();
   console.log('--------------');
 
   process.exit(0);
