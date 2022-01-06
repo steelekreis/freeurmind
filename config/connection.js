c7ffc72f06b27924404b63eb43666f1acd5ab82a
steelekreis <steele.kreis@gmail.com>
2022-01-05 21:07:27 -0700

    fixing server

--- config/connection.js
+++ config/connection.js
@@ -8,9 +8,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306,
-      username: 'root',
-      password: 'VZd4_5xD!m4f9qM'
+      port: 3306
     });
 
 module.exports = sequelize;
