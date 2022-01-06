90704a0e8b436d211f7ba091b787e60786a53941
steelekreis <steele.kreis@gmail.com>
2022-01-05 19:25:56 -0700

    pushing to heroku

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,9 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306
+      port: 3306,
+      username: 'root',
+      password: 'VZd4_5xD!m4f9qM'
     });
 
 module.exports = sequelize;
