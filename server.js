cd9683b6fb9df295e3f48f1d3c393df55acbdd10
steelekreis <steele.kreis@gmail.com>
2022-01-09 02:30:15 -0700

    got rid of helper reference

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(session(sess));
 
 const helpers = require('./utils/helpers');
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
